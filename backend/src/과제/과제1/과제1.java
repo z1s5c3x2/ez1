b43db8da469e22e54c1c286f57b39c90c096278a
504 <504@DESKTOP-SFHCLDM>
2023-07-13 17:26:59 +0900

    .

---
+++ backend/src/과제/과제1/과제1.java
@@ -0,0 +1,29 @@
+package 과제.과제1;
+
+public class 과제1 {
+	
+	public static void main(String[] args)
+	{
+		//1. 4번
+		//2. 2 3 6 7번
+		/*3.
+		 * 1바이트 boolean
+		 * 2바이트 short, char
+		 * 4바이트 int float
+		 * 8바이트 long double
+		 * 4.
+		 * 타입 int double
+		 * 변수이름 age price
+		 * 리터럴 10 3.14
+		 * 5. 3번
+		 * 6. 4번
+		 * 7. 6번
+		 * 8. 3번
+		 * 9. 1번
+		 * 10. 2
+		 * 
+		 * 11. 13 16
+		 *  	
+		 * 		 * */
+	}
+}
